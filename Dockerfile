d54524cc29e31be4b875c2eb6a952196304d9551
jes22santos <jes22santos@gmail.com>
2020-06-22 20:18:01 +0100

    fixing port issue

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,10 @@
 FROM python:3.8.3-slim-buster
-
 RUN mkdir /app
-
 ADD main.py /app
-
+ADD requirements.txt /app
 WORKDIR /app
-
-RUN pip install flask
-
+RUN pip install -r requirements.txt
 ENV FLASK_APP=main.py
-
 EXPOSE 5000
-
+ENV PORT=5000
 CMD flask run
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,4 +11,4 @@ def say_bye():
   return 'Goodbye!'
 
 if __name__ == '__main__':
-    app.run(host ='0.0.0.0', debug = True) 
+    app.run(host ='0.0.0.0', debug = True, port = os.environ.get('PORT')) 
